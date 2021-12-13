b17c8aa135274046ff14f9bda37a10c41c7bc588
Hoang Hieu <you@example.com>
2021-12-13 18:06:21 +0700

    nạp bài 6

---
+++ 6.Tổng quan JavaScript/Thực Hành/hienthidaura/showmess3.js
@@ -0,0 +1,6 @@
+
+
+
+document.querySelector('#btn3').onclick = function btn3() {
+    alert('helo ban')
+};
---
+++ 7. Biến, kiểu dữ liệu và toán tử/Thuc hanh/toantu.js
@@ -0,0 +1,10 @@
+let chieurong;
+let chieudai;
+chieurong=prompt("nhap chieu rong");
+chieudai=prompt("nhap chieu dai");
+
+let rong =parseInt(chieurong);
+let dai= parseInt(chieudai);
+let dientich= rong*dai;
+document.write("dien tich ="+dientich);
+
